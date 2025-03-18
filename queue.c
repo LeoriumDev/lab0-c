7d2229c374b8c75ceb38925e285aa9b937a9e077
LeoriumDev <contact@leorium.com>
2025-03-18 14:17:09 +0800

    Implement q_delete_dup function
    This function deletes all nodes with duplicate strings from the queue.
    
    Returns false if the queue is NULL or empty.
    
    Change-Id: I3915af093961d6c3022e1b72ab9b366a7b3732aa

--- queue.c
+++ queue.c
@@ -120,7 +120,24 @@ bool q_delete_mid(struct list_head *head)
 /* Delete all nodes that have duplicate string */
 bool q_delete_dup(struct list_head *head)
 {
-    // https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+    if (!head || list_empty(head))
+        return false;
+
+    bool is_dup = false;
+    element_t *entry, *safe;
+
+    list_for_each_entry_safe(entry, safe, head, list) {
+        if (&safe->list != head && !strcmp(entry->value, safe->value)) {
+            list_del(&entry->list);
+            q_release_element(entry);
+            is_dup = true;
+        } else if (is_dup) {
+            list_del(&entry->list);
+            q_release_element(entry);
+            is_dup = false;
+        }
+    }
+
     return true;
 }
 
